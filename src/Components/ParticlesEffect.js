c8e93f69fbbcae872ff847669881839d22f6eb23
YimKeul <leesjun29@gmail.com>
2022-10-12 19:45:15 +0900

    feat : add particles

---
+++ src/Components/ParticlesEffect.js
@@ -0,0 +1,208 @@
+import React from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+const ParticlesEffect = () => {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    await loadFull(main);
+  };
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: 0,
+        },
+        particles: {
+          number: {
+            //눈 개수
+            value: 50,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
+          color: {
+            value: "#fff",
+          },
+          shape: {
+            type: "circle",
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false,
+            },
+          },
+          size: {
+            value: 10,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 40,
+              size_min: 0.1,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: false,
+            distance: 500,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 2,
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: "bottom",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200,
+            },
+          },
+        },
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: "bubble",
+            },
+            onclick: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 0.5,
+              },
+            },
+            bubble: {
+              distance: 400,
+              size: 4,
+              duration: 0.3,
+              opacity: 1,
+              speed: 3,
+            },
+            repulse: {
+              distance: 200,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
+            },
+          },
+        },
+        retina_detect: true,
+        background: {
+          color: "#0d47a1",
+          opacity: 0.4,
+          image: "",
+          position: "50% 50%",
+          repeat: "no-repeat",
+          size: "cover",
+        },
+      }}
+    />
+  );
+};
+
+export default ParticlesEffect;
+
+/**
+  options={{
+        background: {
+          color: {
+            value: "#0d47a1",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+
+
+ */
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ParticlesEffect from "../Components/ParticlesEffect";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div>Home screen</div>;
+  return (
+    <S.Container>
+      <ParticlesEffect />
+      <Link to="/Main">
+        <S.Btn>Hello</S.Btn>
+      </Link>
+    </S.Container>
+  );
 };
 export default Home;
+
+const S = {
+  Container: styled.div`
+    background-color: whitesmoke;
+    width: 100%;
+    height: 100%;
+  `,
+
+  Btn: styled.button`
+    color: blue;
+  `,
+};
